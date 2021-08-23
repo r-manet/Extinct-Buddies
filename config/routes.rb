5593afdf298d8f28bcfca8b71755b87f7b94c020
Catherine Jeong <cathjeong@gmail.com>
2021-08-24 01:24:02 +0900

    created the router

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  devise_for :users
+
+  root to: 'animals#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :animals do
+    resources :bookings do
+      resources :reviews
+    end
+  end
 end
