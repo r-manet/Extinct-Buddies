2a1b80e816df1067371835c9397e715719ca5c94
Catherine Jeong <cathjeong@gmail.com>
2021-08-26 21:14:44 +0900

    set up the map for index page

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,8 @@
 class Animal < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
   validates :description, presence: true
   validates :price, presence: true
